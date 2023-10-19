4ea7062aa40c3ab3b3dcc9e19111d7a6b4196880
saikiran560 <82799459+saikiran560@users.noreply.github.com>
2023-10-19 15:04:27 +0530

    Add files via upload

--- string_alphabetical.js
+++ string_alphabetical.js
@@ -1,9 +1,23 @@
-function alphabetical(input) {
-    const sortedString = input.split('').sort().join('');
+function sortStringAlphabetically(input) {
+    input = input.toLowerCase(); // Convert input to lowercase for case-insensitive sorting
+    const characters = input.split(''); // Split the input into an array of characters
+
+    for (let i = 0; i < characters.length; i++) {
+        for (let j = i + 1; j < characters.length; j++) {
+            if (characters[i] > characters[j]) {
+                // Swap characters[i] and characters[j]
+                const temp = characters[i];
+                characters[i] = characters[j];
+                characters[j] = temp;
+            }
+        }
+    }
+
+    const sortedString = characters.join('');
     return sortedString;
 }
 
 const userInput = prompt("Enter a string:");
-const sortedResult = alphabetical(userInput);
+const sortedResult = sortStringAlphabetically(userInput);
 
 console.log("String in alphabetical order: " + sortedResult);
