433786cfef46e57446b4a0269888fa9f7d1a1277
saikiran560 <82799459+saikiran560@users.noreply.github.com>
2023-10-19 09:42:51 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,20 @@
+//printing num from  1 to 10
+let i = 1;
+for(i=1;i<=10;i++){
+    console.log(i);
+}
+
+let num = 2;
+while(num<=10){
+    console.log(num);
+    num += 2;
+}
+
+let text = " "
+let j = 0;
+do{
+    text +=  j;
+    j++;
+}
+while(j<10);
+console.log(text);
