9ea8e240c934c532b62b5b9a89943e709dfed0e8
saikiran560 <82799459+saikiran560@users.noreply.github.com>
2023-11-01 16:45:34 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,20 @@
+//switch case
+let day = "monday"
+
+switch(day){
+    case 'monday':
+    case 'tuesday':
+    case 'wednesday':
+    case 'thursday':
+    case 'fryday':
+        console.log("7am");
+        break;
+    case 'saturday':
+        console.log("11am");
+        break;
+    case 'sanday':
+        console.log("11am");
+        break;
+    default :
+        console.log(" You entered wrong input")
+}
