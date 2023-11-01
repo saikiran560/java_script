dc230676175684046b54a40e54f7e3580413f226
saikiran560 <82799459+saikiran560@users.noreply.github.com>
2023-11-01 16:38:57 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,53 @@
+// data types are two types :- primitive and nonprimitive
+// primitive :- number,string,boolean,null,undefined,symbol
+// nonprimitiv :- object
+
+let data = 8          // number
+let FirstName = "nani"     // string
+
+// in this "n" means bigint data type
+let num1 = 12345678900987654321123456789n
+console.log(num1+2n);
+
+// oparators
+let LastName = "poleboyina"
+console.log(FirstName+LastName)
+
+// escape words
+// "\"nani\" " it ignores the quotes error and print asit is string what ue give
+let TotalName = "poleboyina Nani \"python\" "
+console.log(TotalName)
+
+// "\n" is print new line
+let TotalName1 = "poleboyina \n Nani"
+console.log(TotalName1)
+
+// "/t" it gives the sigle tag space
+let TotalName2 = "poleboyina \t Nani"
+console.log(TotalName2)
+
+// booleans 
+let bool = 5>6
+console.log(bool)
+
+let bool1 = 5<6
+console.log(bool1)
+
+console.log(typeof bool1,bool)
+
+// null 
+let user = null
+console.log(user)
+console.log(typeof user)
+
+//undefined
+let user1 
+console.log(user1)
+
+
+  // datatypes 
+  // variables
+  // objects
+  // loops
+  // conditions
+  // operators
---
+++ function_in_functions
@@ -0,0 +1,18 @@
+// function with in the function :
+function add(a,b){
+    let c = a+b;
+    return c
+};
+
+function sub(x,y){
+    let z = x-y;
+    return z
+};
+
+function Combine(){
+    let c = add(5,5);
+    let z = sub(100,50);
+    //console.log(c,z);
+    return c,z
+};
+combine(add,sub);
